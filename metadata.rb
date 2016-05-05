269b50e449059dfcef28710ae42b1c8f0cd72427
Al Lefebvre <alain.lefebvre@lightspeedhq.com>
2016-05-05 09:19:40 -0400

    DT-916 Updated to allow install from pre-compiled binary

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devtools@lightspeedpos.com'
 license          'all_rights'
 description      'Installs/Configures stats_ag'
 long_description 'Installs/Configures stats_ag'
-version          '0.1.4'
+version          '0.1.5'
 
 
 depends 'golang',     '~> 1.5.1'
